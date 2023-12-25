ead5290ed6add85069c24e32adc33d927ed6bfa8
Albina <62645685+olbenka@users.noreply.github.com>
2023-12-25 23:04:34 +0300

    Delete firstAidKit directory

--- firstAidKit/common-jpa/src/main/java/ru/project/dao/BinaryContentDAO.java
+++
@@ -1,7 +0,0 @@
-package ru.project.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.project.entity.BinaryContent;
-
-public interface BinaryContentDAO extends JpaRepository<BinaryContent, Long> {
-}
--- firstAidKit/node/src/main/java/ru/project/service/AppUserService.java
+++
@@ -1,9 +0,0 @@
-package ru.project.service;
-
-import ru.project.entity.AppUser;
-
-
-public interface AppUserService {
-    String registerUser(AppUser appUser);
-    String setEmail(AppUser appUser, String email);
-}
